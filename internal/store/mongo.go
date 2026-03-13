dc23a1c018baa0270ffaef90ec2e8dec7ff7c8ff
Elad Gildnur <elad.gildnur@gmail.com>
2026-03-13 21:18:13 +0200

    feat: add MongoDB connection helper

---
+++ internal/store/mongo.go
@@ -0,0 +1,36 @@
+package store
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+// DB wraps the MongoDB client and database handle.
+type DB struct {
+	client *mongo.Client
+	db     *mongo.Database
+}
+
+// NewDB connects to MongoDB and returns a DB handle for the "rolebook" database.
+func NewDB(uri string) (*DB, error) {
+	client, err := mongo.Connect(options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, err
+	}
+	return &DB{
+		client: client,
+		db:     client.Database("rolebook"),
+	}, nil
+}
+
+// Collection returns a handle for the named collection.
+func (d *DB) Collection(name string) *mongo.Collection {
+	return d.db.Collection(name)
+}
+
+// Disconnect closes the MongoDB connection.
+func (d *DB) Disconnect(ctx context.Context) error {
+	return d.client.Disconnect(ctx)
+}
